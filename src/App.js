3dc12dc54732065509f1f2358af2b3d52f636f20
derrek-gass <dardcore@gmail.com>
2020-03-02 15:24:50 -0800

    working on toolbox

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import Badges from './Components/Badges';
 
 class App extends Component {
 
@@ -44,24 +45,17 @@ class App extends Component {
     }
 
     render() {
-        return ( <
-            div className = "App" >
-            <
-            Header data = { this.state.resumeData.main }
-            /> <
-            About data = { this.state.resumeData.main }
-            /> <
-            Resume data = { this.state.resumeData.resume }
-            /> <
-            Portfolio data = { this.state.resumeData.portfolio }
-            /> <
-            Testimonials data = { this.state.resumeData.testimonials }
-            /> <
-            Contact data = { this.state.resumeData.main }
-            /> <
-            Footer data = { this.state.resumeData.main }
-            /> <
-            /div>
+        return ( 
+        <div className = "App" >
+            <Header data = { this.state.resumeData.main }/>
+            <About data = { this.state.resumeData.main }/>
+            <Resume data = { this.state.resumeData.resume }/>
+            <Badges data = { this.state.resumeData.badges }/>
+            <Portfolio data = { this.state.resumeData.portfolio }/>
+            <Testimonials data = { this.state.resumeData.testimonials }/>
+            <Contact data = { this.state.resumeData.main }/>
+            <Footer data = { this.state.resumeData.main }/>
+        </div>
         );
     }
 }
---
+++ src/Components/Badges.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Badges extends Component {
+  render() {
+
+    if(this.props.data){
+      var badges = this.props.data.badges.map(function(badges){
+        var skillImage = 'images/badges/'+badges.image;
+        return <div key={badges.title} className="columns badges-item">
+           <div className="item-wrap">
+               <img alt={badges.title} src={skillImage} />
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="badges">
+      <div className="row toolbox">
+         <div className="three columns header-col">
+            <h1><span>Toolbox</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+                <div id="badges-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              
+            <div className="row item">  
+                  {badges}
+                  </div>
+               </div>
+            </div>
+         </div>
+  
+   </section>
+    );
+  }
+}
+
+export default Badges;
