074a02cb7cf436e63007e20d1474a9d91ef2fb2c
derrek-gass <dardcore@gmail.com>
2021-02-20 15:02:27 -0800

    adding course descriptions

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,7 +7,9 @@ class Resume extends Component {
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+          <p>{education.description[0]}</p>
+          <p>{education.description[1]}</p>
+        </div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
