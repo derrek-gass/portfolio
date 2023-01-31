90cb9f493da3523e52b158218af6ec684ea00d9f
derrek-gass <dardcore@gmail.com>
2023-01-31 03:27:25 -0800

    bump pkg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
-import Badges from './Components/Badges';
+import 'App.css';
+import Header from "Components/Header";
+import About from "Components/About";
+import Resume from "Components/Resume";
+import Badges from "Components/Badges";
+import Portfolio from "Components/Portfolio";
+import Testimonials from "Components/Testimonials";
+import Footer from "Components/Footer";
 
+// TODO: Convert this to functional
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            foo: 'bar',
             resumeData: {}
         };
 
