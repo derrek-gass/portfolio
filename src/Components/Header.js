15c52ab8222cbd951ea26ff10dd75dae211bb680
Derrek Gass <dardcore@gmail.com>
2023-09-14 18:01:39 -0700

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -32,8 +32,9 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a fully remote <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">{name}:</h1>
+            <h3>An extremely dedicated <span>{occupation}</span></h3>
+            <h3>{description}</h3>
             <hr />
             <ul className="social">
                {networks}
