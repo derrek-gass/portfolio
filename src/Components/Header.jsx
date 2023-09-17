f6abf31a577c79d8f552c80e710e78941d67bb31
Derrek Gass <dardcore@gmail.com>
2023-09-17 14:20:33 -0700

    goofy but it's my website lol

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -33,8 +33,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">{name}:</h1>
-            <h3>An extremely dedicated <span>{occupation}</span></h3>
-            <h3>{description}</h3>
+                <h3>An extremely dedicated <span>{occupation}</span><br />{description}</h3>
             <hr />
             <ul className="social">
                {networks}
