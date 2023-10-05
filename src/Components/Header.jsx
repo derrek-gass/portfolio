c1aadb2b76143ace08676e9d6b4e88a768e2b46a
derrekgass <dardcore@gmail.com>
2023-10-04 20:21:57 -0700

    make header functional component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-
-class Header extends Component {
-	render() {
-		if (this.props.data) {
-			var occupation = this.props.data.occupation;
-			var description = this.props.data.description;
-			var myName = this.props.data.name;
-			var networks = this.props.data.social.map(function (network) {
-				return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-			})
-		}
+function Header(props) {
+	if (props.data) {
+		const { occupation, description, name: myName, social } = props.data;
+		var networks = social.map(function (network) {
+			return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+		})
 		return (
 			<header id="home">
 				<nav id="nav-wrap">
